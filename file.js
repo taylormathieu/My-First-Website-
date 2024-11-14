1ea66f42f2ac8bf3b0022b7a2a2810d6f68aa9d6
taylormathieu <taylor.mathieu@edu.sait.ca>
2024-11-14 13:48:22 -0700

    Adding first bit of js seeing if its working

--- file.js
+++ file.js
@@ -1,4 +1,30 @@
 "use strict";
 
-console.log('it works');
+const fYear = document.getElementById("footerYear");
+const d = new Date();
+fYear.textContent = d.getFullYear();
+
+let name = prompt("Hello! What's your name?");
+alert("Nice to meet you, " + name + "!");
+
+const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+    alert('Button clicked');
+});
+
+const greeting = () => {
+    if (d.getHours() <= 11) {
+        alert('Good Morning!');
+    } else if (d.getHours() <18 ) {
+        alert('Good Afternoon');
+    } else {
+        alert('Good Evening');
+    }
+}
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click, greeting");
+
+
+
  
